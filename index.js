2c40d43a614c4007ecb8b2bc9269a60976714942
Alok <alok@Aloks-MacBook-Air.local>
2022-10-25 03:19:47 +0600

    api data added

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ const port = process.env.PORT || 3001;
 
 app.use(cors());
 
+const categories = require('./Data/Categories.json');
+
 app.get('/', (req, res) => {
     res.send('server is running');
+})
+app.get('/categories', (req, res) => {
+    res.send(categories);
 })
 
 app.listen(port, () => console.log(`Edule server is running on port ${port}`));
