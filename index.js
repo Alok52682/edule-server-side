b6f539376a934a3cc21d318e060675905291bb26
Alok <alok@Aloks-MacBook-Air.local>
2022-10-25 03:41:06 +0600

    server is completed also deployed

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 
 const categories = require('./Data/Categories.json');
+const courses = require('./Data/Courses.json');
 
 app.get('/', (req, res) => {
     res.send('server is running');
@@ -14,5 +15,26 @@ app.get('/', (req, res) => {
 app.get('/categories', (req, res) => {
     res.send(categories);
 })
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourses = courses.find(category => category.id === id);
+    res.send(selectedCourses);
+})
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (id === '6') {
+        res.send(courses);
+    }
+    else {
+        const category = courses.filter(category => category.category_id === id);
+        res.send(category);
+    }
+
+})
+
 
 app.listen(port, () => console.log(`Edule server is running on port ${port}`));
