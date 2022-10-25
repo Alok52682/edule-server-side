3393111cd1116444b04fc50520db8c5438a597e1
Alok <alok@Aloks-MacBook-Air.local>
2022-10-25 19:00:48 +0600

    server complete

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ app.get('/', (req, res) => {
 app.get('/courses', (req, res) => {
     res.send(courses);
 })
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const course = courses.find(course => course.id === id);
+    res.send(course);
+})
 
 
 app.listen(port, () => console.log(`Edule server is running on port ${port}`));
