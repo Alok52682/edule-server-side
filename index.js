2bf01d2b66184e16ff76777d83df6c95ba49cad8
Alok <alok@Aloks-MacBook-Air.local>
2022-10-26 19:48:46 +0600

    forget to save file in previous commit

--- index.js
+++ index.js
@@ -6,14 +6,20 @@ const port = process.env.PORT || 3001;
 
 app.use(cors());
 
+// get courses from couses.jsom file
 const courses = require('./Data/Courses.json');
+
 
 app.get('/', (req, res) => {
     res.send('server is running');
 })
+
+// send allcourses to the link
 app.get('/courses', (req, res) => {
     res.send(courses);
 })
+
+// send specific course to link by id
 app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
     const course = courses.find(course => course.id === id);
